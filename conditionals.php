dcc568d79385ba458a52465d5d0bef03c7896d2c
matthew.moulds1@gmail.com <matthew.moulds1@gmail.com>
2015-10-17 17:27:51 +0100

    New file for conditions using the if , else operator

---
+++ conditionals.php
@@ -0,0 +1,47 @@
+<?php
+define("USE_FULL_NAME",FALSE);
+define("MAX_BADGES", 150000);
+
+$firstName = "Matthew";
+$lastName = "Moulds";
+$location = "Orlando, FL";
+$role = "SysAdmin";
+
+if( USE_FULL_NAME == TRUE ) {
+  // run this code if USE_FULL_NAME is true
+  $name = $firstName . ' ' . $lastName;
+} else {
+  $name = $firstName;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8>
+  	<title><?php echo $name ?> | Treehouse Profile</title>
+  	<link href="css/style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <section class="sidebar text-center">
+      <div class="avatar">
+        <img src="img/avatar.png" alt="<?php echo $name ?>">
+      </div>
+      <h1><?php echo $name ?></h1>
+      <p><?php echo $location ?></p>
+      <hr />
+      <p>Welcome to PHP Basics!</p>
+      <hr />
+      <ul class="social">
+        <li><a href=""><span class="icon twitter"></span></a></li>
+      </ul>
+    </section>
+    <section class="main">
+      <pre><?php
+
+        ?></pre>
+    </section>
+  </body>
+</html>
